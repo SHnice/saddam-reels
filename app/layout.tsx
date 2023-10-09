76df951c58a956b4e31df712bfa9db5104945e41
Saddam Hussain <arsal0344@gmail.com>
2023-10-09 15:56:33 -0700

    add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'TikTok Clone',
-  description: 'TikTok Clone',
+  title: 'Saddam Reels',
+  description: 'Saddam Reels is a social media platform for sharing short videos.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
