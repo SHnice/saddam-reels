8cc0f5b67693b06c3cabad2422fc5ac8b5543f96
Saddam Hussain <arsal0344@gmail.com>
2023-10-09 16:48:59 -0700

    added nav

--- app/components/PostMain.tsx
+++ app/components/PostMain.tsx
@@ -18,20 +18,20 @@ export default function PostMain({ post }: PostMainCompTypes) {
             let observer = new IntersectionObserver((entries) => {
                 entries[0].isIntersecting ? video.play() : video.pause()
             }, { threshold: [0.6] });
-        
+
             observer.observe(postMainElement);
         }
     }, [])
 
     return (
         <>
-            <div id={`PostMain-${post.id}`} className="flex border-b py-6">
+            <div id={`PostMain-${post.id}`} className="flex border-b pb-6">
 
-                <div className="cursor-pointer">
+                <div className="hidden lg:block">
                     <img className="rounded-full max-h-[60px]" width="60" src={useCreateBucketUrl(post?.profile?.image)} />
                 </div>
 
-                <div className="pl-3 w-full px-4">
+                <div className="lg:pl-3 pl-0 w-full px-4">
                     <div className="flex items-center justify-between pb-0.5">
                         <Link href={`/profile/${post.profile.user_id}`}>
                             <span className="font-bold hover:underline cursor-pointer">
@@ -39,38 +39,40 @@ export default function PostMain({ post }: PostMainCompTypes) {
                             </span>
                         </Link>
 
-                        <button className="border text-[15px] px-[21px] py-0.5 border-[#F02C56] text-[#F02C56] hover:bg-[#ffeef2] font-semibold rounded-md">
+                        {/* <button className="border text-[15px] px-[21px] py-0.5 border-[#F02C56] text-[#F02C56] hover:bg-[#ffeef2] font-semibold rounded-md">
                             Follow
-                        </button>
+                        </button> */}
                     </div>
                     <p className="text-[15px] pb-0.5 break-words md:max-w-[400px] max-w-[300px]">{post.text}</p>
-                    <p className="text-[14px] text-gray-500 pb-0.5">#fun #cool #SuperAwesome</p>
+                    {/* <p className="text-[14px] text-gray-500 pb-0.5">#fun #cool #SuperAwesome</p> */}
                     <p className="text-[14px] pb-0.5 flex items-center font-semibold">
-                        <ImMusic size="17"/>
+                        {/* <ImMusic size="17"/>
                         <span className="px-1">original sound - AWESOME</span>
-                        <AiFillHeart size="20"/>
+                        <AiFillHeart size="20"/> */}
                     </p>
 
                     <div className="mt-2.5 flex">
                         <div
-                            className="relative min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
+                            className="relative w-[300px] min-h-[480px] max-h-[580px] max-w-[260px] flex items-center bg-black rounded-xl cursor-pointer"
                         >
-                            <video 
+                            <video
                                 id={`video-${post.id}`}
                                 loop
                                 controls
                                 autoPlay
-                                className="rounded-xl object-cover mx-auto h-full" 
+                                className="rounded-xl object-cover mx-auto h-full"
                                 src={useCreateBucketUrl(post?.video_url)}
                             />
-                            <img 
-                                className="absolute right-2 bottom-10" 
-                                width="90" 
+                            <img
+                                className="absolute right-2 bottom-10"
+                                width="90"
                                 src="/images/tiktok-logo-white.png"
                             />
                         </div>
-                        
-                        <PostMainLikes post={post} />
+                        <div className="hidden lg:flex">
+                            <PostMainLikes post={post} />
+
+                        </div>
                     </div>
                 </div>
             </div>
